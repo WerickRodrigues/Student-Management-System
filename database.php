7107010b9b1a59c541bdde6562c91954a57187ec
Werick Rodrigues <166259862+WerickRodrigues@users.noreply.github.com>
2024-07-23 19:08:18 -0300

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,6 @@
 <?php
-// Cria uma nova conexão SQLite
 $pdo = new PDO('sqlite:students.db');
 
-// Cria a tabela de alunos, se não existir
 $pdo->exec("CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY, 
     name TEXT, 
